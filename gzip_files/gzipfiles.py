6e92c8674d8397c2d395aebdb77ca212eda0c50a
Dan Sheffner <Dan@Sheffner.com>
2019-12-10 05:51:46 -0600

    first pass at a script that will gzip files in parellel based on CPU count

---
+++ gzip_files/gzipfiles.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) Dan Sheffner Digital Imaging Software Solutions, INC
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
+"""
+This program will gzip in a parellel files from the file system individually
+make sure you pass "*.json" with quote marks or this will not work!
+./gzipfiles.py "*.json"
+"""
+
+import glob
+import sys
+from multiprocessing import Pool, cpu_count
+import subprocess
+import os
+import sys
+import timeit
+
+
+def processfiles(command1):
+    filename = command1.split(" ")
+    print ("compressing " + filename[-1])
+    subprocess.run(command1, shell=True)
+
+if __name__ == "__main__":
+
+
+    start = timeit.default_timer()
+
+    files = glob.glob(sys.argv[1])
+
+    commands = []
+
+    # build list of commands
+    for each in files:
+        commands.append("gzip -9 " + each)
+    
+    num_of_cpu = cpu_count()
+
+    # simulate smaller server/pool if needed
+    # num_of_cpu = 2
+
+    pool = Pool(processes=num_of_cpu)
+    pool.map(processfiles, commands)
+    
+    stop = timeit.default_timer()
+
+    print ('Total time in seconds: ', stop - start)  
+    print ("gzipfiles.py completed")
