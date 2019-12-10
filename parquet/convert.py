05b744ad5502c0a59577df2f43217e5cfb96a989
Dan Sheffner <Dan@Sheffner.com>
2019-12-10 08:44:55 -0600

    fixing this again

--- parquet/convert.py
+++ parquet/convert.py
@@ -45,7 +45,6 @@ if __name__ == "__main__":
     for each in files:
         print ("converting file " + each + " to parquet format...")
         filename = each.split(".")
-        filename1 = filename
         filename1 = "./" + sys.argv[1] + "-" + filename[0] + ".parquet" 
         dataDF = spark.read.json(each)
         dataDF.write.parquet(filename1)
