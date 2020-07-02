32a899e24eefbe8ee9b7ca22a4617cf94095094f
Dan Sheffner <Dan@Sheffner.com>
2020-07-02 06:47:40 -0500

    leaves behind the ubuntu containers correctly

--- docker/cleanDockerSome.sh
+++ docker/cleanDockerSome.sh
@@ -2,9 +2,11 @@
 echo "cleanDockerSome.sh started..."
 
 output1=$(docker ps | grep "apt-cacher-ng" | awk '{ print $1 }')
+output2=$(docker images | grep "apt-cacher-ng" | awk '{ print $3 }')
+output3=$(docker images | grep "ubuntu                       20.04" | awk '{ print $3 }')
 
 docker stop $(docker ps -aq | grep -v $output1)
 docker rm $(docker ps -aq | grep -v $output1)
-docker image prune -a -f
+docker rmi $(docker images -q | grep -v $output2 | grep -v $output3)
 
 echo "cleanDockerSome.sh completed."
