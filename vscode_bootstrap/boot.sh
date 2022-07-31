36d52d0fd6f54d7cf2417a5b086c4459bb20a72c
Dan Sheffner <dan@sheffner.com>
2022-07-30 19:39:44 -0500

    adding this "-y" I missed.

--- vscode_bootstrap/boot.sh
+++ vscode_bootstrap/boot.sh
@@ -153,7 +153,7 @@ nodejs_install() {
         sudo apt-get install -y nodejs
         curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
         echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-        sudo apt-get update && sudo apt-get install yarn
+        sudo apt-get update && sudo apt-get -y install yarn
     else
         echo "nodejs already looks to be installed..."
     fi
